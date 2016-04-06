ac986e151b6c59c5fe48f7e89d981bb1af74a2db
hyz1011088 <hyz1011088@gmail.com>
2016-04-06 17:21:21 +0800

    property

--- A6-OOP_Super/use_slots.py
+++ A6-OOP_Super/use_slots.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 class Student(object):
-    __slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称
+    __slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称,即为限制实例的属性
 
 class GraduateStudent(Student):
     pass
