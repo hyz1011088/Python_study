f3feaed1c3a1fab174ad0c29b7d2fd2d6a501373
hyz1011088 <hyz1011088@gmail.com>
2016-04-10 19:57:16 +0800

    processing

--- A9-Processing&Thread/do_fork.py
+++ A9-Processing&Thread/do_fork.py
@@ -7,7 +7,7 @@ print('Process (%s) start...' % os.getpid())
 # Only works on Unix/Linux/Mac:
 pid = os.fork()
 if pid == 0:
-    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))  #os.getpid()是子进程的id, os.getppid()是父进程的id
 else:
     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
 
